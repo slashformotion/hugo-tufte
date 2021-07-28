1c7e325bcab6b49628b5ca02809dd9648bba4c25
slashformotion <slashformotion@users.noreply.github.com>
2021-07-28 09:02:44 +0000

    deploy: 0299aa32d307c5584e6a281f02bed54b0affefe5

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-<script>
-    renderMathInElement(
+renderMathInElement(
     document.body,
-        {
+    {
         delimiters: [
-                    {left: "$$", right: "$$", display: true},
-                    {left: "\\[", right: "\\]", display: true},
-                    {left: "$", right: "$", display: true},
-                    {left: "\\(", right: "\\)", display: true}
-                ],
-        displayMode: true 
-        }
-    );
-</script>
+            { left: "$$", right: "$$", display: true },
+            { left: "\\[", right: "\\]", display: true },
+            { left: "$", right: "$", display: false },
+            { left: "\\(", right: "\\)", display: false }
+        ],
+        displayMode: true
+    }
+);
